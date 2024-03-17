d805f3e70dcc3455b5b345e89a12608fc5f544ca
kareem-gafer10 <kareemgafer28.com>
2024-03-17 14:28:36 +0200

    delete comment app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import { productList } from "./data";
 
-// const App = () => {
 
-//   return (
-//     <div className=" container">
-//       <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md ">
-//         {renderProductList}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
 
 import ProductCard from "./components/ProductCard";
 
